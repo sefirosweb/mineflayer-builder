4adc4c7d4b4e5409b9602045232c4541a477b21e
Sefirosweb <sefirosweb@gmail.com>
2023-02-05 00:03:10 +0100

    Refact

--- lib/go_block.ts
+++ lib/go_block.ts
@@ -1,23 +1,23 @@
-//@ts-nocheck
+import { Bot } from "mineflayer"
+import { Vec3 } from "vec3"
 import { wait } from "./helper"
-
-
-const { goals, Movements } = require('mineflayer-pathfinder')
+import { goals, Movements } from 'mineflayer-pathfinder'
 const blocksCanBeReplaced = ['air', 'cave_air', 'lava', 'water', 'bubble_column', 'seagrass', 'tall_seagrass', 'kelp_plant']
 const blockForPlace = ['stone', 'cobblestone', 'dirt', 'andesite', 'diorite', 'granite', 'grass_block']
 
-export default (bot) => {
+export default (bot: Bot) => {
     const mcData = require('minecraft-data')(bot.version)
     const movements = new Movements(bot, mcData)
     movements.digCost = 10
-    movements.maxDropDown = 3
+    movements.maxDropDown = 5
+    //@ts-ignore
     bot.pathfinder.searchRadius = 10
 
-    async function goBlock(position) {
-        const block = bot.blockAt(position)
+    async function goBlock(position: Vec3) {
         const downBlock = bot.blockAt(position.offset(0, -1, 0))
+        if (downBlock === null) throw new Error('down block not found!')
 
-        await placeHelperblocks(downBlock.position)
+        await placeHelperblocks()
 
         const newPosition = position.offset(0.5, 0, 0.5)
         console.log(newPosition)
@@ -26,12 +26,11 @@ export default (bot) => {
         await wait(100)
     }
 
-    async function placeHelperblocks(position) {
+    async function placeHelperblocks() {
         const itemToPlace = bot.inventory.items().find(item => blockForPlace.includes(item.name))
         if (!itemToPlace) {
             throw new Error('No items helper to place')
         }
-
     }
 
     return {
