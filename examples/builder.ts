490ea5e93fb93f3a4a04500c107556c480de24eb
Sefirosweb <sefirosweb@gmail.com>
2023-02-04 19:41:59 +0100

    Refact to TS

--- examples/builder.ts
+++ examples/builder.ts
@@ -1,8 +1,8 @@
 //@ts-nocheck
+import { builder, Build } from 'mineflayer-builder'
+import { Schematic } from 'prismarine-schematic'
 const path = require('path')
 const fs = require('fs').promises
-const { builder, Build } = require('mineflayer-builder')
-const { Schematic } = require('prismarine-schematic')
 const { pathfinder } = require('mineflayer-pathfinder')
 const mineflayer = require('mineflayer')
 const { Vec3 } = require('vec3')
@@ -18,14 +18,14 @@ const bot = mineflayer.createBot({
 bot.loadPlugin(pathfinder)
 bot.loadPlugin(builder)
 
-function wait(ms) { return new Promise(resolve => setTimeout(resolve, ms)) }
+function wait(ms: number) { return new Promise(resolve => setTimeout(resolve, ms)) }
 
 bot.once('spawn', async () => {
 
   bot.chat('/fill 25 -60 25 -25 -50 -25 air')
   bot.chat('/time set day')
 
-  bot.on('path_update', (r) => {
+  bot.on('path_update', (r: any) => {
     const path = [bot.entity.position.offset(0, 0.5, 0)]
     for (const node of r.path) {
       path.push({ x: node.x, y: node.y + 0.5, z: node.z })
@@ -35,13 +35,8 @@ bot.once('spawn', async () => {
   while (!bot.entity.onGround) {
     await wait(100)
   }
-  bot.on('messagestr', (message, messagePosition, jsonMsg) => {
-    if (message.includes('start')) {
-      start()
-    }
-  })
 
-  bot.on('chat', async (username, message) => {
+  bot.on('chat', async (username: string, message: string) => {
     console.info(username, message)
     if (message.startsWith('build')) {
       const [, schematicName] = message.split(' ')
@@ -62,43 +57,24 @@ bot.once('spawn', async () => {
   build('chest.schem')
 })
 
-async function build(name) {
+async function build(name: string) {
   const schematicName = !name.endsWith('.schem') ? name + '.schem' : name
-  const filePath = path.resolve(path.join(__dirname, '..', 'schematics', schematicName))
+  const filePath: string = path.resolve(path.join(__dirname, '..', 'schematics', schematicName))
   if (!fileExists(filePath)) {
     bot.chat(`File ${schematicName} not found`)
     return
   }
+
   const schematic = await Schematic.read(await fs.readFile(filePath), bot.version)
   const at = new Vec3(0, -60, 0)
   // bot.entity.position.floored()
   bot.chat(`Building at ${at.x} ${at.y} ${at.z}`)
   const build = new Build(schematic, bot.world, at)
   bot.chat('/fill 416 122 -324 415 122 -324 minecraft:air')
-  bot.builder.build(build, noMaterial)
-}
-
-async function noMaterial(item, resolve, reject) {
-  console.info('Building interrupted missing', item?.name)
-  reject()
-}
-
-async function start() {
-  bot.chat('/clear')
-  await wait(1000)
-  bot.chat('/give builder dirt')
-  await wait(1000)
-  bot.chat('/fill 187 4 122 209 30 101 air')
-  await wait(1000)
-  bot.chat('/tp 197 4 121')
-  await wait(1000)
-  const at = bot.entity.position.floored()
-  // console.log('Building at ', at)
-  const build = new Build(schematic, bot.world, at)
   bot.builder.build(build)
 }
 
-async function fileExists(path) {
+async function fileExists(path: string) {
   try {
     await fs.promises.access(path)
     return true
